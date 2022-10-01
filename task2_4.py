a815725dbe8750ceba0b43608a4949ca00a64d51
Ivan Maestro <masterov_in@mail.ru>
2022-10-01 14:14:02 +0300

    update task 4 - deepcopy

--- task2_4.py
+++ task2_4.py
@@ -1,6 +1,7 @@
 # 4.Реализуйте алгоритм перемешивания списка.
 
 import random
+import copy
 
 # 1 вариант
 
@@ -17,11 +18,18 @@ import random
 
 # 2 вариант
 
-n = int(input('Enter Number of elements: '))
-first_list = [i for i in range(n)]
-second_list = first_list[:]
-for i in range(n):
-    index_random = random.randint(0, n - 1)
-    second_list[i], second_list[index_random] = second_list[index_random],second_list[i]
-print('-> ', first_list)
-print('-> ', second_list)
+# n = int(input('Enter Number of elements: '))
+# first_list = [i for i in range(n)]
+# second_list = first_list[:]
+# for i in range(n):
+#     index_random = random.randint(0, n - 1)
+#     second_list[i], second_list[index_random] = second_list[index_random],second_list[i]
+# print('-> ', first_list)
+# print('-> ', second_list)
+
+# # 3 Вариант deepcopy - тренировка
+# my_list = [[1, 2], [3, 4]]
+# my_list2 = copy.deepcopy(my_list)
+# my_list[0].append(5)
+# print(my_list)
+# print(my_list2)
